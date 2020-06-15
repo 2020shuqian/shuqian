25503462350c85dd72deb9a27fb85affda5a7de3
2020shuqian <66657593+2020shuqian@users.noreply.github.com>
2020-06-15 21:48:54 +0800

    what you upate for this time

--- learn_more/panel.js
+++ learn_more/panel.js
@@ -7,7 +7,7 @@ var rnd = Math.random();
         }else {
             var msg ='要小心預料之外的麻煩....';
         }
-        window.alert('歡迎蒞臨星座運勢線上占卜 ' + msg);  //對話框顯示占卜結果      
+       // window.alert('歡迎蒞臨星座運勢線上占卜 ' + msg);  //對話框顯示占卜結果      
 
         
 //開合框設定
@@ -16,10 +16,14 @@ $(document).ready(function () {
   $('#panel dt')
     .click(function (e) {
       var d = $('+dd', this);
+      //console.log(typeof(d));
       if (d.css('display') === 'block') {
-        d.slideUp(1000);
+        //d.slideUp(1000);
+        d.toggle("slow");
+        //d.slideToggle();
       } else {
-        d.slideDown(1000);
+        //d.slideDown(1000);
+        d.toggle("slow");
       }
     });
 }); 
@@ -31,9 +35,9 @@ $(function() {
   $('#dd4').html("土象星座"); 
 });
 $(function() {  
-    $('#dt1').html("具有人際溝通以及社交關係的聰慧能力，在某些地方，會表現出強烈的意志力以及聰明的特質。"+'</p>'+'<a href="index.html">風象星座(智慧型)：雙子座、天秤座、水瓶座。</a>'+'</p>'+'<img src="learn_more/images/02.jpg" width=70%>');  
-    $('#dt2').html("通常表現出某種追求領導地位的欲望，會有獨斷、積極、熱心、具創造力以及陽剛的特質。"+'</p>'+'<a href="index.html">火象星座(精力型)：牡羊座、獅子座、射手座。</a>'+'</p>'+'<img src="learn_more/images/03.jpg" width=70%>');
-    $('#dt3').html("注重情緒及情感的領域，並且和生命中較敏感、憑直覺以及更深一層心靈研究的事物有關。"+'</p>'+'<a href="index.html">水象星座(情感型)：巨蟹座、天蠍座、雙魚座。</a>'+'</p>'+'<img src="learn_more/images/01.jpg" width=70%>');
-    $('#dt4').html("擁有非常務實的態度，並具有善於使用及管理物質、金錢等，這些使人類生活安定運作的必須能力。"+'</p>'+'<a href="index.html">土象星座(實際型)：金牛座、處女座、魔羯座。</a>'+'</p>'+'<img src="learn_more/images/04.jpg" width=70%>');
+    $('#dtt1').html("具有人際溝通以及社交關係的聰慧能力，在某些地方，會表現出強烈的意志力以及聰明的特質。"+'</p>'+'<a href="index.html">風象星座(智慧型)：雙子座、天秤座、水瓶座。</a>'+'</p>'+'<img src="learn_more/images/02.jpg" width="70%">');  
+    $('#dtt2').html("通常表現出某種追求領導地位的欲望，會有獨斷、積極、熱心、具創造力以及陽剛的特質。"+'</p>'+'<a href="index.html">火象星座(精力型)：牡羊座、獅子座、射手座。</a>'+'</p>'+'<img src="learn_more/images/03.jpg" width="70%">');
+    $('#dtt3').html("注重情緒及情感的領域，並且和生命中較敏感、憑直覺以及更深一層心靈研究的事物有關。"+'</p>'+'<a href="index.html">水象星座(情感型)：巨蟹座、天蠍座、雙魚座。</a>'+'</p>'+'<img src="learn_more/images/01.jpg" width="70%">');
+    $('#dtt4').html("擁有非常務實的態度，並具有善於使用及管理物質、金錢等，這些使人類生活安定運作的必須能力。"+'</p>'+'<a href="index.html">土象星座(實際型)：金牛座、處女座、魔羯座。</a>'+'</p>'+'<img src="learn_more/images/04.jpg" width="70%">');
   });
           
